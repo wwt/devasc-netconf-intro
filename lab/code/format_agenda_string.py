335e7c215b60537059833b4586ffbcd98c484cdd
Timothy Hull <timothyhull@gmail.com>
2020-10-04 21:15:31 -0700

    Initial commit

---
+++ lab/code/format_agenda_string.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import xmltodict
+from pprint import pprint
+
+data = '''
+<?xml version="1.0" ?>
+ <root>
+  <agenda>
+   <items>
+    <item>NETCONF Protocol</item>
+    <item>NETCONF Tools</item>
+    <item>Hands-On Lab</item>
+    <item>Q &amp; A</item>
+    <item>Homework</item>
+   </items>
+  </agenda>
+ </root>
+ '''
+
+xml_data = data.strip()
+py_data = xmltodict.parse(xml_data, dict_constructor=dict)
+
+print()
+print('** Raw XML Data **')
+print(xml_data)
+print()
+
+print('** Python Data **')
+pprint(py_data)
+print()
+
+print('** "item" Python Object Type **')
+print(type(py_data['root']['agenda']['items']['item']))
+print()
