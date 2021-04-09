c4315933caf53c15e837f7670bf8df227c0ecd97
Timothy Hull <timothyhull@gmail.com>
2021-04-09 15:09:08 -0700

    setup_yang_suite function development and exception handling enhancements

--- config/setup_lab.ps1
+++ config/setup_lab.ps1
@@ -3,6 +3,9 @@
 # Constants
 $FRAME = "-" * 49
 $S3_BUCKET_URI = "https://devasc-netconf.s3-us-west-2.amazonaws.com/"
+$DOCKER_COMPOSE_FILE = "docker-compose.yml"
+$DOCKER_COMPOSE_URI = $S3_BUCKET_URI + $DOCKER_COMPOSE_FILE
+$DOCKER_REGISTRY_PATH = "lab-docker.wwtatc.com/devnet/"
 $DOCKER_SETTINGS_FILE = "settings.json"
 $DOCKER_SETTINGS_URI = $S3_BUCKET_URI + $DOCKER_SETTINGS_FILE
 $DOCKER_SETTINGS_FILE_PATH = "c:\Users\admin\AppData\Roaming\Docker\"
@@ -13,6 +16,12 @@ $JUPYTER_S3_BUCKET_URI = "https://jupyter-launcher.s3-us-west-2.amazonaws.com/"
 $JUPYTER_SCRIPT = "jupyter_launcher.ps1"
 $JUPYTER_SCRIPT_URI = $JUPYTER_S3_BUCKET_URI + $JUPYTER_SCRIPT
 $ROOT_PATH = "C:\Users\admin"
+$YANG_SUITE_IMAGES = @(
+    "${DOCKER_REGISTRY_PATH}yang-suite-backup-1",
+    "${DOCKER_REGISTRY_PATH}yang-suite-nginx-1",
+    "${DOCKER_REGISTRY_PATH}yang-suite-yangsuite-1"
+)
+$YANG_SUITE_REPO = "https://github.com/CiscoDevNet/yangsuite"
 $YANG_SUITE_URL = "http://localhost"
 
 
@@ -44,19 +53,26 @@ function display_intro() {
 }
 
 
+# Verify the Docker service and Windows process are running
 function docker_status() {
-    Write-Host "Checking Docker service & process status..." -NoNewline -ForegroundColor Green
-    $docker_service_status = Get-Service -DisplayName "Docker*" | Where-Object {$_.Status -eq "Running"}
-    $docker_process_status = docker info | Select-String -Pattern 'error' | ForEach-Object {$_.Matches.Success}
-    Write-Host "done." -ForegroundColor Green
-    Write-Host ""
+    try {
+        Write-Host "Checking Docker service & process statuses..." -NoNewline -ForegroundColor Green
+        Write-Host ""
+        $docker_service_status = Get-Service -DisplayName "Docker*" | Where-Object {$_.Status -eq "Running"}
+        $docker_process_status = docker info | Select-String -Pattern 'error' | ForEach-Object {$_.Matches.Success}
+        Write-Host "done." -ForegroundColor Green
+        Write-Host ""
 
-    if (-not ($docker_service_status)) {
-        handle_error("Docker Desktop Windows service not running. `nPlease wait for the service to start and try again.")
+        if (-not ($docker_service_status)) {
+            handle_error("Docker Desktop Windows service not running. `nPlease wait for the service to start and try again.")
+        }
+
+        if ($docker_process_status) {
+            handle_error("Docker Desktop process not running. `nPlease make sure Docker Desktop is running.")
+        }
     }
-
-    if ($docker_process_status) {
-        handle_error("Docker Desktop process not running. `nPlease make sure Docker Desktop is running.")
+    catch {
+        handle_error("Unable to determine the Docker service and process statuses , please try again.")
     }
 }
 
@@ -64,55 +80,75 @@ function docker_status() {
 # Windows preparation
 function setup_windows() {
     # Disable Windows Updates & delete temporary update files
-    if (-not (Test-Path .winupdate -PathType leaf)) {
-        Write-Host "Disabling Windows Update..." -NoNewline -ForegroundColor Green
-        Start-Process PowerShell -WindowStyle Minimized -Verb Runas -ArgumentList "Stop-Service wuauserv; Set-Service -Name wuauserv -StartupType Disabled; Get-ChildItem -Path C:\WINDOWS\SoftwareDistribution\Download -Verbose | Remove-Item -Force -Confirm:`$false -Recurse -ErrorAction SilentlyContinue"
-        Out-File -FilePath .winupdate
-        Write-Host "done." -ForegroundColor Green
-        Write-Host ""
+    try {
+        if (-not (Test-Path .winupdate -PathType leaf)) {
+            Write-Host "Disabling Windows Update..." -NoNewline -ForegroundColor Green
+            Write-Host ""
+            Start-Process PowerShell -WindowStyle Minimized -Verb Runas -ArgumentList "Stop-Service wuauserv; Set-Service -Name wuauserv -StartupType Disabled; Get-ChildItem -Path C:\WINDOWS\SoftwareDistribution\Download -Verbose | Remove-Item -Force -Confirm:`$false -Recurse -ErrorAction SilentlyContinue"
+            Out-File -FilePath .winupdate
+            Write-Host "done." -ForegroundColor Green
+            Write-Host ""
+        }
+    }
+    catch {
+        handle_error("An error occurred disabling Windows Update, please try again.")
     }
 }
 
 
 # Docker Desktop preparation
 function setup_docker() {
-    if (!(Test-Path .dockerclean -PathType leaf)) {
-        # Disable Docker Desktop updates
-        Write-Host "Disabling Docker Desktop updates..." -NoNewline -ForegroundColor Green
-        $settings_file = $DOCKER_SETTINGS_FILE_PATH + $DOCKER_SETTINGS_FILE
-        Rename-Item -Path $settings_file -NewName "${settings_file}.old" -ErrorAction SilentlyContinue
-        Invoke-WebRequest -Uri $DOCKER_SETTINGS_URI -OutFile $settings_file
-        Write-Host "done." -ForegroundColor Green
-        Write-Host ""
+    try {
+        if (!(Test-Path .dockerclean -PathType leaf)) {
+            # Disable Docker Desktop updates
+            Write-Host "Disabling Docker Desktop updates..." -NoNewline -ForegroundColor Green
+            Write-Host ""
+            $settings_file = $DOCKER_SETTINGS_FILE_PATH + $DOCKER_SETTINGS_FILE
+            Rename-Item -Path $settings_file -NewName "${settings_file}.old" -ErrorAction SilentlyContinue
+            Invoke-WebRequest -Uri $DOCKER_SETTINGS_URI -OutFile $settings_file
+            Write-Host "done." -ForegroundColor Green
+            Write-Host ""
 
-        # Remove existing Docker images
-        Write-Host "Removing existing Docker Images..." -ForegroundColor Green
-        docker rmi -f $(docker image ls -aq); docker system prune -af --volumes
-        Out-File -FilePath .dockerclean
-        Write-Host ""
-        Write-Host "done." -ForegroundColor Green
-        Write-Host ""
+            # Remove existing Docker images
+            Write-Host "Removing existing Docker Images..." -ForegroundColor Green
+            Write-Host ""
+            docker rmi -f $(docker image ls -aq); docker system prune -af --volumes
+            Out-File -FilePath .dockerclean
+            Write-Host ""
+            Write-Host "done." -ForegroundColor Green
+            Write-Host ""
+        }
+    }
+    catch {
+        handle_error("A Docker setup error occurred, please try again.")
     }
 }
 
 
 # Run Jupyter Launcher
 function run_jupyter_launcher() {
-    Write-Host ""
+    # Download and execute Jupyter Launcher
     Write-Host "Downloading scripts..." -NoNewline -ForegroundColor Green
-    # Download .repo file
-    Invoke-WebRequest -Uri $REPO_FILE_URI -OutFile $REPO_FILE
-
-    # Download Jupyter Launcher Script
-    Invoke-WebRequest -Uri $JUPYTER_SCRIPT_URI -OutFile $JUPYTER_SCRIPT
-
-    # Run Jupyter Launcher Script
-    Invoke-Expression .\$JUPYTER_SCRIPT
-    Write-Host "done." -ForegroundColor Green
     Write-Host ""
+    try {
+        # Download .repo file
+        Invoke-WebRequest -Uri $REPO_FILE_URI -OutFile $REPO_FILE
+
+        # Download Jupyter Launcher Script
+        Invoke-WebRequest -Uri $JUPYTER_SCRIPT_URI -OutFile $JUPYTER_SCRIPT
+
+        # Run Jupyter Launcher Script
+        Invoke-Expression .\$JUPYTER_SCRIPT
+        Write-Host "done." -ForegroundColor Green
+        Write-Host ""
+    }
+    catch {
+        handle_error("Unable to download scripts, please try again.")
+    }
 }
 
 
+# Validate the Git clone operation of the lab repo
 function validate_git_repo() {
     Set-Location $ROOT_PATH
     if (-not (Test-Path $REPO_NAME -PathType Container)) {
@@ -123,16 +159,72 @@ function validate_git_repo() {
 
 # Setup YANG Suite
 function setup_yang_suite() {
+    # Clone Yang Suite repo
+    Write-Host "Cloning YANG Suite repository..." -ForegroundColor Green
+    Write-Host ""
+    try {
+        $yang_suite_dir = Test-Path "${ROOT_PATH}\yangsuite" -PathType Container
+
+        if (-not ($yang_suite_dir)) {
+            docker exec -it jupyter1 git clone $YANG_SUITE_REPO
+        }
+        Write-Host "done." -ForegroundColor Green
+        Write-Host ""
+    }
+    catch {
+        handle_error("Unable to clone YANG Suite repository, please try again.")
+    }
+
     # Pull YANG Suite Images
+    Write-Host "Loading ${YANG_SUITE_IMAGES.length} YANG Suite Docker Images..." -NoNewline -ForegroundColor Green
+    Write-Host ""
+    try {
+        $loop_count = 1
+        foreach ($image in $YANG_SUITE_IMAGES) {
+            Write-Host ""
+            Write-Host "Pulling Image ${loop_count} of ${YANG_SUITE_IMAGES.length}"
+                docker pull $image
+            Write-Host ""
+            $loop_count += 1
+        }
+        Write-Host "done." -ForegroundColor Green
+        Write-Host ""
+    }
+    catch {
+        handle_error("Unable to pull YANG Suite Images, please try again.")
+    }
 
-    # Import YANG Suite Images
+    # Import custom Docker Compose file
+    Write-Host "Importing Docker Compose file..." -NoNewline -ForegroundColor Green
+    Write-Host ""
+    try {
+        $docker_compose_file = "${yang_suite_dir}\docker" + $DOCKER_COMPOSE_FILE
+        Rename-Item -Path $docker_compose_file -NewName "${docker_compose_file}.old" -ErrorAction SilentlyContinue
+        Invoke-WebRequest -Uri $DOCKER_COMPOSE_URI -OutFile $docker_compose_file
+        Write-Host "done." -ForegroundColor Green
+        Write-Host ""
+    }
+    catch {
+        handle_error("Unable to load Docker Compose file, please try again.")
+    }
 
-    # Start YANG Suite Images
-
-    # Import YANG Suite self-signed certificate
+    # Start YANG Suite Containers
+    Write-Host "Starting YANG Suite Containers..." -NoNewline -ForegroundColor Green
+    Write-Host ""
+    try {
+        Set-Location "${yang_suite_dir}\docker"
+        docker-compose up -d
+        Start-Sleep -Seconds 10
+        Write-Host "done." -ForegroundColor Green
+        Write-Host ""
+    }
+    catch {
+        handle_error("Unable to start YANG Suite Containers, please try again.")
+    }
     
     # Launch YANG Suite in Chrome
     Write-Host "Opening YANG Suite..." -NoNewline -ForegroundColor Green
+    Write-Host ""
     try {
         Start-Process "chrome.exe" "${YANG_SUITE_URL}"
         Write-Host "done." -ForegroundColor Green
@@ -141,37 +233,6 @@ function setup_yang_suite() {
         handle_error("Unable to launch Chrome, you may manually navigate to the URL: `n${YANG_SUITE_URL}.")
     }
 }
-
-
-# # Setup ANX
-# function setup_anx() {
-#     $anx_dir = Test-Path "${ROOT_PATH}\anx" -PathType Container
-
-#     # Clone ANX repo
-#     if (-not ($anx_dir)) {
-#         Write-Host "Cloning ANX repository..." -ForegroundColor Green
-#         Write-Host ""
-#         docker exec -it jupyter1 git clone https://github.com/cisco-ie/anx.git
-#     }
-
-#     # Build & start ANX
-#     Set-Location "${ROOT_PATH}\anx"
-#     docker-compose up -d
-#     Start-Sleep -Seconds 10
-
-#     Write-Host "done." -ForegroundColor Green
-#     Write-Host ""
-
-#     # Launch ANX in Chrome
-#     Write-Host "Opening ANX..." -NoNewline -ForegroundColor Green
-#     try {
-#         Start-Process "chrome.exe" "${ANX_URL}"
-#         Write-Host "done." -ForegroundColor Green
-#     }
-#     catch {
-#         handle_error("Unable to launch Chrome, you may manually navigate to the URL: `n${ANX_URL}.")
-#     }
-# }
 
 
 # Create link file object
