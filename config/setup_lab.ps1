b92398b02394850275236955ba57445abada06d8
Timothy Hull <timothyhull@gmail.com>
2021-04-12 10:55:33 -0700

    Improve detail of Docker process instructions

--- config/setup_lab.ps1
+++ config/setup_lab.ps1
@@ -79,13 +79,13 @@ function docker_status() {
             # }
 
             # If Docker Desktop process does not respond, attempt to start
-            # Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-            # Write-Host "Docker Desktop process not running, attempting restart..." -NoNewline -ForegroundColor Yellow
-            # Start-Sleep 15
-            # Write-Host "done." -ForegroundColor Yellow
-            # Write-Host
+            Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+            Write-Host "Docker Desktop process not running, attempting restart..." -NoNewline -ForegroundColor Yellow
+            Start-Sleep 15
+            Write-Host "done." -ForegroundColor Yellow
+            Write-Host
 
-            handle_error("The Docker Desktop process encountered an unrecoverable error. `nPlease launch a new copy of the lab and try again.")
+            handle_error("Docker Desktop process was not running, restart automatically attempted. `nPlease try again.  If the problem persists, launch a new copy of the lab.")
         }
     }
     catch {
